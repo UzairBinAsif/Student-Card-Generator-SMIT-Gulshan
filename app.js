b6aaf92ab5e7499491b17fc6b0de3b00cb8bb262
UzairBinAsif <shaikhuzairasif14@gmail.com>
2025-10-25 11:11:23 +0500

    init

---
+++ app.js
@@ -0,0 +1,60 @@
+function showCard() {
+    event.preventDefault()
+    let studentName = document.getElementById("studentName")
+    let studentEmail = document.getElementById("studentEmail")
+    let studentNumber = document.getElementById("studentNumber")
+    let studentRoll = document.getElementById("studentRoll")
+    let studentCourse = document.getElementById("studentCourse")
+    let studentImage = document.getElementById("studentImage")
+
+    let studentNameSpan = document.getElementById("studentNameSpan")
+    let studentEmailSpan = document.getElementById("studentEmailSpan")
+    let studentNumberSpan = document.getElementById("studentNumberSpan")
+    let studentRollSpan = document.getElementById("studentRollSpan")
+    let studentCourseSpan = document.getElementById("studentCourseSpan")
+    let studentImageDiv = document.getElementById("studentImageDiv")
+
+    if (studentName.value.length && studentEmail.value.length && studentNumber.value.length && studentRoll.value.length && studentCourse.value.length) {
+        if (studentImage.files.length) {
+            let imageURL = URL.createObjectURL(studentImage.files[0])
+            
+            studentNameSpan.innerText = studentName.value
+            studentEmailSpan.innerText = studentEmail.value
+            studentNumberSpan.innerText = studentNumber.value
+            studentRollSpan.innerText = studentRoll.value
+            studentCourseSpan.innerText = studentCourse.value
+            studentImageDiv.style.backgroundImage = `url("${imageURL}")`
+            
+            document.getElementsByClassName("right")[0].style.display = "block"
+        } else {
+            let timerInterval;
+            Swal.fire({
+                title: "Error!",
+                html: "Please Provide an Image.",
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                }
+            })
+        } 
+    } else {
+        let timerInterval;
+        Swal.fire({
+            title: "Error!",
+            html: "Please fill all the required fields.",
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        })
+    }
+
+}
